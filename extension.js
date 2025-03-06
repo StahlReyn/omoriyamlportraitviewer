73816df9613c764f8dc04e202b2c3cc97e611327
StahlReyn <reynstahl@gmail.com>
2025-03-06 18:58:45 -0500

    plugin

--- extension.js
+++ extension.js
@@ -1,36 +1,123 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// Import necessary modules
+const fs = require('fs');
+const path = require('path');
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// Initialize the default tooltip size
+let currentTooltipSize = 0;
+
+// Define available tooltip sizes
+const tooltipSizes = [100, 200, 300, 400, 500, 0];
 
 /**
- * @param {vscode.ExtensionContext} context
+ * Activates the extension
  */
 function activate(context) {
+	console.log("ACTIVATE PROVIDER")
+	vscode.window.showInformationMessage('Hello World!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "omoriyamlportraitviewer" is now active!');
+	// Register command to open image in the editor
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extension.openImageYaml', (imgPath) => {
+			vscode.commands.executeCommand('vscode.open', vscode.Uri.file(imgPath));
+		})
+	);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('omoriyamlportraitviewer.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	// Register command to resize image in the tooltip
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extension.resizeImageYaml', async ({ imgPath, size }) => {
+			currentTooltipSize = size === 0 ? 0 : parseInt(size, 10);
+			await vscode.window.showInformationMessage('Changes will only take effect after reopening the tooltip due to VS Code limitations.', { modal: true });
+		})
+	);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from omoriYamlPortraitViewer!');
-	});
-
-	context.subscriptions.push(disposable);
+	// Register hover providers for all supported languages
+	context.subscriptions.push(
+		vscode.languages.registerHoverProvider('yaml', {
+			provideHover(document, position) {
+				return provideHover(document, position);
+			}
+		})
+	);
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
+/**
+ * Provides hover content when the user hovers over a comment
+ */
+function provideHover(document, position) {
+	if (document.languageId != 'yaml') { return; }
 
-module.exports = {
-	activate,
-	deactivate
+	const lineText = document.lineAt(position.line).text;
+	return processComment(lineText, document);
 }
+
+// Processes the comment text to determine if it contains an image comment.
+function processComment(commentText, document) {
+	const commentPattern = /.*?faceset: ([\w\d]*)/;
+	const match = commentText.match(commentPattern);
+	if (!match) { return; }
+
+	const imagePath = match[1];
+	if (!imagePath) { return; }
+
+	const cleanImagePath = "../../img/faces/" + imagePath.trim() + ".png";
+	const documentFolderPath = path.dirname(document.uri.fsPath);
+	let imgPath = path.join(documentFolderPath, cleanImagePath);
+
+	let imgExists = false;
+
+	try {
+		imgExists = fs.existsSync(imgPath);
+	} catch (error) {
+		imgExists = false;
+	}
+
+	if (imgExists) {
+		const imgUri = vscode.Uri.file(imgPath).toString();
+
+		// Generate size adjustment links
+		const sizeLinks = tooltipSizes.map(size =>
+		{
+			const displaySize = size === 0 ? 'No Scale' : `${size}px`;
+			if (size === currentTooltipSize)
+			{
+				return `**[${displaySize}](command:extension.resizeImageYaml?${encodeURIComponent(JSON.stringify({ imgPath, size }))})**`;
+			}
+
+			return `[${displaySize}](command:extension.resizeImageYaml?${encodeURIComponent(JSON.stringify({ imgPath, size }))})`;
+
+		}).join(' ');
+
+		// Construct the hover content
+		const hoverContent = [
+			'## Omori Faceset',
+			'',
+			`[Open Image in IDE](command:extension.openImageYaml?${encodeURIComponent(JSON.stringify(imgPath))})`,
+			'',
+			sizeLinks,
+			'',
+			currentTooltipSize === 0
+				? `![Image](${imgUri})`
+				: `![Image](${imgUri}|width=${currentTooltipSize}px)`,
+		].join('\n');
+
+		// Create and return the hover object
+		const markdown = new vscode.MarkdownString(hoverContent, true);
+		markdown.isTrusted = true;
+		return new vscode.Hover(markdown);
+	} else {
+		// If the image does not exist, display an error message
+		const hoverContent = [
+			'## Image Comments',
+			`Could not find image file.`,
+			'',
+			`**Path**: ${imgPath}`,
+		].join('\n');
+
+		const markdown = new vscode.MarkdownString(hoverContent, true);
+		markdown.isTrusted = true;
+		return new vscode.Hover(markdown);
+	}
+}
+
+exports.activate = activate;
